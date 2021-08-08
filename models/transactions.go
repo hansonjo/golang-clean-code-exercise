9df4d0ba02856016521e48d9c10a1217bbbf3ee8
hansonjo <66669373+hansonjo@users.noreply.github.com>
2021-08-08 09:51:03 +0700

    'ref'

--- models/transactions.go
+++ models/transactions.go
@@ -9,11 +9,35 @@ type transactions struct {
 func NewTransactions(d []int) Transactions {
         data := make(map[int]int)
 
+        for i, v := range d {
+                data[i] = v
+        }
+
         return &transactions{data: data}
 }
 
 // TODO: implement this
 // i.e. go test is green
 func (t *transactions) Get(idx int) int {
-        return 0
+        return t.data[idx]
+}
+
+// TODO: implement this method
+// i.e. go test is green
+func (t *transactions) GetTotal() int {
+        salesTotal := 0
+        for i := 0; i <= 6; i++ {
+                salesTotal += t.Get(i)
+        }
+        return salesTotal
+}
+
+// TODO: implement this method
+// i.e. go test is green
+func (t *transactions) GetTotalWithinRange(i, j int) int {
+        salesTotal := 0
+        for i := i; i <= j; i++ {
+                salesTotal += t.Get(i)
+        }
+        return salesTotal
 }
